972d251f353e82e6f9598ef925d74086be592cc1
Nightmare <admin@nightmarecoin.com>
2014-08-18 14:13:10 -0600

    Commit

---
+++ public/src/js/config.js
@@ -0,0 +1,67 @@
+'use strict';
+
+//Setting up route
+angular.module('insight').config(function($routeProvider) {
+  $routeProvider.
+    when('/block/:blockHash', {
+      templateUrl: '/views/block.html',
+      title: 'Nightmare Block '
+    }).
+    when('/block-index/:blockHeight', {
+      controller: 'BlocksController',
+      templateUrl: '/views/redirect.html'
+    }).
+    when('/tx/:txId/:v_type?/:v_index?', {
+      templateUrl: '/views/transaction.html',
+      title: 'Nightmare Transaction '
+    }).
+    when('/', {
+      templateUrl: '/views/index.html',
+      title: 'Home'
+    }).
+    when('/blocks', {
+      templateUrl: '/views/block_list.html',
+      title: 'Nightmare Blocks solved Today'
+    }).
+    when('/blocks-date/:blockDate/:startTimestamp?', {
+      templateUrl: '/views/block_list.html',
+      title: 'Nightmare Blocks solved '
+    }).
+    when('/address/:addrStr', {
+      templateUrl: '/views/address.html',
+      title: 'Nightmare Address '
+    }).
+    when('/status', {
+      templateUrl: '/views/status.html',
+      title: 'Status'
+    })
+    .otherwise({
+      templateUrl: '/views/404.html',
+      title: 'Error'
+    });
+});
+
+//Setting HTML5 Location Mode
+angular.module('insight')
+  .config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+  })
+  .run(function($rootScope, $route, $location, $routeParams, $anchorScroll, ngProgress) {
+    $rootScope.$on('$routeChangeStart', function() {
+      ngProgress.start();
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function() {
+      ngProgress.complete();
+
+      //Change page title, based on Route information
+      $rootScope.titleDetail = '';
+      $rootScope.title = $route.current.title;
+      $rootScope.isCollapsed = true;
+      $rootScope.currentAddr = null;
+
+      $location.hash($routeParams.scrollTo);
+      $anchorScroll();
+    });
+  });
